2fa620dc995a1a61c0e74b800ff9e8f294379975
Johnson Joseph <jhonsonj88@gmail.com>
2019-09-04 03:51:15 +0000

    Automatically backed up by Learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,6 +3,10 @@ class SearchesController < ApplicationController
   def search
   end
 
+  def friends
+      foursquare = FoursquareService.new
+      @friends = foursquare.friends(session[:token])
+    end
   
   def foursquare
     client_id = "CO3LIXJPH1LYAC5OOTLKLJE334NVDIYG24KUFOVEQ22WVYDP"
